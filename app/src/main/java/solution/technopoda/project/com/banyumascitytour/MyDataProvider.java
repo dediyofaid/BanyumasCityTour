ef0c9a0f60cc681267f0434cdafc4131b1395b72
devtechnpoda <dev.team.technopoda@gmail.com>
2015-05-12 14:36:15 +0700

    Di isi data array ne

--- app/src/main/java/solution/technopoda/project/com/banyumascitytour/MyDataProvider.java
+++ app/src/main/java/solution/technopoda/project/com/banyumascitytour/MyDataProvider.java
@@ -10,18 +10,12 @@ import java.util.List;
 public class MyDataProvider {
     public static HashMap<String, List<String>> getDataHashMap() {
         HashMap<String, List<String>> pendidikanHashMap = new HashMap<String, List<String>>();
-        HashMap<String, List<String>> kesehatanHashMap = new HashMap<String, List<String>>();
-        HashMap<String, List<String>> kulinerHashMap = new HashMap<String, List<String>>();
-        HashMap<String, List<String>> wisataHashMap = new HashMap<String, List<String>>();
-        HashMap<String, List<String>> biroPerjalanan = new HashMap<String, List<String>>();
 
         List<String> perguruantinggiList = new ArrayList<String>();
         List<String> smaList = new ArrayList<String>();
         List<String> smpList = new ArrayList<String>();
 
-        List<String> BanyumasList = new ArrayList<String>();
-        List<String> MargonoList = new ArrayList<String>();
-        List<String> HidayahList = new ArrayList<String>();
+
 
 
 
@@ -38,31 +32,39 @@ public class MyDataProvider {
         for (int i = 0; i < MyDataArrays.smpArray.length; i++) {
             smpList.add(MyDataArrays.smpArray[i]);
         }
-
-
-        for (int i = 0; i < MyDataArrays.perguruantinggiArray.length; i++) {
-            perguruantinggiList.add(MyDataArrays.perguruantinggiArray[i]);
-        }
-
-        for (int i = 0; i < MyDataArrays.smaArray.length; i++) {
-            smaList.add(MyDataArrays.smaArray[i]);
-        }
-
-        for (int i = 0; i < MyDataArrays.smpArray.length; i++) {
-            smpList.add(MyDataArrays.smpArray[i]);
-        }
-
-
 
 
 
         pendidikanHashMap.put("Perguruan Tinggi", perguruantinggiList);
         pendidikanHashMap.put("SMA/SMK/MAN", smaList);
         pendidikanHashMap.put("SMP/MTS", smpList);
-
         return pendidikanHashMap;
+        //--------------------------------------------------------------------------
 
 
+    }
+    public static HashMap<String, List<String>> kesehatanDataHashMap() {
+        HashMap<String, List<String>> kesehatanHashMap = new HashMap<String, List<String>>();
+        List<String> banyumasList = new ArrayList<String>();
+        List<String> margonoList = new ArrayList<String>();
+        List<String> hidayahList = new ArrayList<String>();
 
+
+        for (int i = 0; i < MyDataArrays.banyumasArray.length; i++) {
+            banyumasList.add(MyDataArrays.banyumasArray[i]);
+        }
+
+        for (int i = 0; i < MyDataArrays.margonoArray.length; i++) {
+            margonoList.add(MyDataArrays.margonoArray[i]);
+        }
+
+        for (int i = 0; i < MyDataArrays.hidayahArray.length; i++) {
+            hidayahList.add(MyDataArrays.hidayahArray[i]);
+        }
+
+        kesehatanHashMap.put("RSUD Banyumas", banyumasList);
+        kesehatanHashMap.put("RSUD Margono Soekarjo", margonoList);
+        kesehatanHashMap.put("RSUD Hidayah", hidayahList);
+        return kesehatanHashMap;
     }
 }
