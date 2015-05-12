0f4de972fe3507eeec42d41ae9121960cfc6ae20
habibwisnup <mtc.altair@gmail.com>
2015-05-12 14:33:55 +0700

    UPdate bejaddd, mantafff

--- app/src/main/java/solution/technopoda/project/com/banyumascitytour/Pendidikan.java
+++ app/src/main/java/solution/technopoda/project/com/banyumascitytour/Pendidikan.java
@@ -1,5 +1,6 @@
 package solution.technopoda.project.com.banyumascitytour;
 
+<<<<<<< HEAD
 import android.app.Activity;
 import android.content.Intent;
 import android.graphics.Typeface;
@@ -8,36 +9,38 @@ import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.ActionBarActivity;
 import android.util.TypedValue;
+=======
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+>>>>>>> UPdate bejaddd, mantafff
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.app.ExpandableListActivity;
-import android.view.Gravity;
-import android.widget.AbsListView;
 import android.widget.BaseExpandableListAdapter;
+<<<<<<< HEAD
 import android.widget.Button;
 import android.widget.ExpandableListAdapter;
+=======
+>>>>>>> UPdate bejaddd, mantafff
 import android.widget.ExpandableListView;
 import android.widget.TextView;
 
-import android.widget.ExpandableListView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * Created by HabibDea on 29/04/2015.
  */
 public class Pendidikan extends Fragment {
+<<<<<<< HEAD
+=======
+
+>>>>>>> UPdate bejaddd, mantafff
     View rootview;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater,  ViewGroup container,  Bundle savedInstanceState) {
         rootview = inflater.inflate(R.layout.pendidikan,container,false);
+<<<<<<< HEAD
         Button pendidikan = (Button)rootview.findViewById(R.id.btnPendidikan);
         pendidikan.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -46,6 +49,11 @@ public class Pendidikan extends Fragment {
                 startActivity(i);
             }
         });
+=======
+
+>>>>>>> UPdate bejaddd, mantafff
         return rootview;
     }
 }
+
+
