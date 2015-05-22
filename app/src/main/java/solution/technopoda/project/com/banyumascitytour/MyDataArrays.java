31377bb4c3e753048b75c9c8cd06fc6dd5ccbd6c
devtechnpoda <dev.team.technopoda@gmail.com>
2015-05-22 20:24:57 +0700

    jad

--- app/src/main/java/solution/technopoda/project/com/banyumascitytour/MyDataArrays.java
+++ app/src/main/java/solution/technopoda/project/com/banyumascitytour/MyDataArrays.java
@@ -7,24 +7,19 @@ public class MyDataArrays {
     static String[] perguruantinggiArray = {"Universitas Jendral Soedirman", "Universitas Muhammadiyah Purwokerto", "Universitas Wijaya Kusuma", "STIMIK AMIKOM"};
     static String[] smaArray = {"SMA 1 Purwokerto", "SMA 2 Purwokerto", "SMK Bina Teknologi", "SMK Bunda Satria"};
     static String[] smpArray = {"SMP Negeri 1 Purwokerto", "SMP Negeri 1 Wangon", "SMP Muhammadiyah 1", "MTS Ma'arif NU 1"};
+    static String[] sdArray = {"SDN 1 Kober", "SDN 1 Bodas", "SDN 1 Wlahar Wetan", "SDN 1 Berkoh", "SDN 1 Arcawinangun"};
 
-    static String[] banyumasArray = {"Universitas Jendral Soedirman", "Universitas Muhammadiyah Purwokerto", "Universitas Wijaya Kusuma", "STIMIK AMIKOM"};
-    static String[] margonoArray = {"SMA 1 Purwokerto", "SMA 2 Purwokerto", "SMK Bina Teknologi", "SMK Bunda Satria"};
-    static String[] hidayahArray = {"SMP Negeri 1 Purwokerto", "SMP Negeri 1 Wangon", "SMP Muhammadiyah 1", "MTS Ma'arif NU 1"};
+    static String[] rsPemerintahArray = {"RSUD Banyumas", "RSUD Margono Soekarjo", "RSUD Ajibarang"};
+    static String[] rsSwastaArray = {"RS Islam", "RS Elisabeth", "RS Sinar Kasih", "RS Bunda", "RS Ananda", "RS Medica Lestari"};
 
-    static String[] atriumArray = {"Universitas Jendral Soedirman", "Universitas Muhammadiyah Purwokerto", "Universitas Wijaya Kusuma", "STIMIK AMIKOM"};
-    static String[] greenvalleyArray = {"SMA 1 Purwokerto", "SMA 2 Purwokerto", "SMK Bina Teknologi", "SMK Bunda Satria"};
-    static String[] astonArray = {"SMP Negeri 1 Purwokerto", "SMP Negeri 1 Wangon", "SMP Muhammadiyah 1", "MTS Ma'arif NU 1"};
+    static String[] berbintangArray = {"Atrium Resort Hotel", "Green Valley Resort", "Hotel Ardi Kencana", "Hotel Aston", "Hotel Borobudur"};
+    static String[] melatiArray = {"Hotel Anggrek", "Hotel Asri", "Hotel Astro", "Hotel Cahaya Nirwana", "Hotel Cendrawasih"};
 
-    static String[] wisataAlamArray = {"Universitas Jendral Soedirman", "Universitas Muhammadiyah Purwokerto", "Universitas Wijaya Kusuma", "STIMIK AMIKOM"};
-    static String[] wisataReligiArray = {"SMA 1 Purwokerto", "SMA 2 Purwokerto", "SMK Bina Teknologi", "SMK Bunda Satria"};
-    static String[] wisataSejarahArray = {"SMP Negeri 1 Purwokerto", "SMP Negeri 1 Wangon", "SMP Muhammadiyah 1", "MTS Ma'arif NU 1"};
+    static String[] tamanRekreasiArray = {"Alun-alun Purwokerto","Taman Andhang Pangrenan", "Taman Balai Kumambang"};
+    static String[] wisataAlamArray = {"Baturaden Adventure Forrest", "Batur Agung", "Lokawisata Baturaden", "Bendung Gerak Serayu", "Fatmaba Waterpark"};
+    static String[] wisataReligiArray = {"Masjid Saka Tunggal", "Goa Maria Kaliori", "Masjid Nur Sulaiman"};
 
-    static String[] buntosArray = {"Universitas Jendral Soedirman", "Universitas Muhammadiyah Purwokerto", "Universitas Wijaya Kusuma", "STIMIK AMIKOM"};
-    static String[] gethukArray = {"SMA 1 Purwokerto", "SMA 2 Purwokerto", "SMK Bina Teknologi", "SMK Bunda Satria"};
-    static String[] paparonArray = {"SMP Negeri 1 Purwokerto", "SMP Negeri 1 Wangon", "SMP Muhammadiyah 1", "MTS Ma'arif NU 1"};
+    static String[] kulinerArray = {"Bakso Pekih", "Bebek Goreng H.Slamet", "Buntos Chicken & Rita Bakery", "Getuk Goreng H.Tohirin Sokaraja", "Paparon's Pizza Purwokerto", "Iga Bakar Mas Giri"};
+    static String[] biroArray = {"Amanat tour", "Arjuna tour & travel", "CV Satria Tour","Exelent tour", "Karisma tour & travel", "Surya Sarana Wisata"};
 
-    static String[] amanatArray = {"Universitas Jendral Soedirman", "Universitas Muhammadiyah Purwokerto", "Universitas Wijaya Kusuma", "STIMIK AMIKOM"};
-    static String[] arjunaArray = {"SMA 1 Purwokerto", "SMA 2 Purwokerto", "SMK Bina Teknologi", "SMK Bunda Satria"};
-    static String[] mitraArray = {"SMP Negeri 1 Purwokerto", "SMP Negeri 1 Wangon", "SMP Muhammadiyah 1", "MTS Ma'arif NU 1"};
 }
--- app/src/main/java/solution/technopoda/project/com/banyumascitytour/MyDataProvider.java
+++ app/src/main/java/solution/technopoda/project/com/banyumascitytour/MyDataProvider.java
@@ -14,6 +14,7 @@ public class MyDataProvider {
         List<String> perguruantinggiList = new ArrayList<String>();
         List<String> smaList = new ArrayList<String>();
         List<String> smpList = new ArrayList<String>();
+        List<String> sdList = new ArrayList<String>();
 
 
         for (int i = 0; i < MyDataArrays.perguruantinggiArray.length; i++) {
@@ -28,11 +29,16 @@ public class MyDataProvider {
             smpList.add(MyDataArrays.smpArray[i]);
         }
 
+        for (int i = 0; i < MyDataArrays.sdArray.length; i++){
+            sdList.add(MyDataArrays.sdArray[i]);
+        }
+
 
 
         pendidikanHashMap.put("Perguruan Tinggi", perguruantinggiList);
         pendidikanHashMap.put("SMA/SMK/MAN", smaList);
         pendidikanHashMap.put("SMP/MTS", smpList);
+        pendidikanHashMap.put("SD/MI/SLB", sdList);
         return pendidikanHashMap;
         //--------------------------------------------------------------------------
 
@@ -40,62 +46,56 @@ public class MyDataProvider {
     }
     public static HashMap<String, List<String>> kesehatanDataHashMap() {
         HashMap<String, List<String>> kesehatanHashMap = new HashMap<String, List<String>>();
-        List<String> banyumasList = new ArrayList<String>();
-        List<String> margonoList = new ArrayList<String>();
-        List<String> hidayahList = new ArrayList<String>();
+        List<String> rsPemerintahList = new ArrayList<String>();
+        List<String> rsSwastaList = new ArrayList<String>();
 
 
-        for (int i = 0; i < MyDataArrays.banyumasArray.length; i++) {
-            banyumasList.add(MyDataArrays.banyumasArray[i]);
+        for (int i = 0; i < MyDataArrays.rsPemerintahArray.length; i++) {
+            rsPemerintahList.add(MyDataArrays.rsPemerintahArray[i]);
         }
 
-        for (int i = 0; i < MyDataArrays.margonoArray.length; i++) {
-            margonoList.add(MyDataArrays.margonoArray[i]);
+        for (int i = 0; i < MyDataArrays.rsSwastaArray.length; i++) {
+            rsSwastaList.add(MyDataArrays.rsSwastaArray[i]);
         }
 
-        for (int i = 0; i < MyDataArrays.hidayahArray.length; i++) {
-            hidayahList.add(MyDataArrays.hidayahArray[i]);
-        }
-
-        kesehatanHashMap.put("RSUD Banyumas", banyumasList);
-        kesehatanHashMap.put("RSUD Margono Soekarjo", margonoList);
-        kesehatanHashMap.put("RSUD Hidayah", hidayahList);
+        kesehatanHashMap.put("Rumah Sakit Pemerintah", rsPemerintahList);
+        kesehatanHashMap.put("Rumah Sakit Swasta", rsSwastaList);
         return kesehatanHashMap;
     }
 
 
     public static HashMap<String, List<String>> hotelDataHashMap() {
         HashMap<String, List<String>> hotelHashMap = new HashMap<String, List<String>>();
-        List<String> atriumresorthotelList = new ArrayList<String>();
-        List<String> greenvalleyList = new ArrayList<String>();
-        List<String> astonList = new ArrayList<String>();
+        List<String> berbintangList = new ArrayList<String>();
+        List<String> melatiList = new ArrayList<String>();
 
 
-        for (int i = 0; i < MyDataArrays.atriumArray.length; i++) {
-            atriumresorthotelList.add(MyDataArrays.atriumArray[i]);
+        for (int i = 0; i < MyDataArrays.berbintangArray.length; i++) {
+            berbintangList.add(MyDataArrays.berbintangArray[i]);
         }
 
-        for (int i = 0; i < MyDataArrays.greenvalleyArray.length; i++) {
-            greenvalleyList.add(MyDataArrays.greenvalleyArray[i]);
+        for (int i = 0; i < MyDataArrays.melatiArray.length; i++) {
+            melatiList.add(MyDataArrays.melatiArray[i]);
         }
 
-        for (int i = 0; i < MyDataArrays.astonArray.length; i++) {
-            astonList.add(MyDataArrays.astonArray[i]);
-        }
 
-        hotelHashMap.put("Atrium Resort Hotel", atriumresorthotelList);
-        hotelHashMap.put("Green Valley Hotel", greenvalleyList);
-        hotelHashMap.put("Aston Imperium", astonList);
+
+        hotelHashMap.put("Hotel Berbintang", berbintangList);
+        hotelHashMap.put("Hotel Melati", melatiList);
         return hotelHashMap;
     }
 
 
     public static HashMap<String, List<String>> wisataDataHashMap() {
         HashMap<String, List<String>> wisataHashMap = new HashMap<String, List<String>>();
+        List<String> tamanRekreasiList = new ArrayList<String>();
         List<String> wisataAlamList = new ArrayList<String>();
         List<String> wisataReligiList = new ArrayList<String>();
-        List<String> wisataSejarahList = new ArrayList<String>();
 
+
+        for (int i = 0; i < MyDataArrays.tamanRekreasiArray.length; i++) {
+            tamanRekreasiList.add(MyDataArrays.tamanRekreasiArray[i]);
+        }
 
         for (int i = 0; i < MyDataArrays.wisataAlamArray.length; i++) {
             wisataAlamList.add(MyDataArrays.wisataAlamArray[i]);
@@ -105,63 +105,37 @@ public class MyDataProvider {
             wisataReligiList.add(MyDataArrays.wisataReligiArray[i]);
         }
 
-        for (int i = 0; i < MyDataArrays.wisataSejarahArray.length; i++) {
-            wisataSejarahList.add(MyDataArrays.wisataSejarahArray[i]);
-        }
-
+        wisataHashMap.put("Taman Rekreasi", tamanRekreasiList);
         wisataHashMap.put("Wisata Alam", wisataAlamList);
         wisataHashMap.put("Wisata Religi", wisataReligiList);
-        wisataHashMap.put("Wisata Sejarah", wisataSejarahList);
         return wisataHashMap;
     }
 
     public static HashMap<String, List<String>> kulinerDataHashMap() {
         HashMap<String, List<String>> kulinerHashMap = new HashMap<String, List<String>>();
-        List<String> buntosList = new ArrayList<String>();
-        List<String> gethukList = new ArrayList<String>();
-        List<String> paparonList = new ArrayList<String>();
+        List<String> kulinerList = new ArrayList<String>();
 
-
-        for (int i = 0; i < MyDataArrays.buntosArray.length; i++) {
-            buntosList.add(MyDataArrays.buntosArray[i]);
+        for (int i = 0; i < MyDataArrays.kulinerArray.length; i++) {
+            kulinerList.add(MyDataArrays.kulinerArray[i]);
         }
 
-        for (int i = 0; i < MyDataArrays.gethukArray.length; i++) {
-            gethukList.add(MyDataArrays.gethukArray[i]);
-        }
 
-        for (int i = 0; i < MyDataArrays.paparonArray.length; i++) {
-            paparonList.add(MyDataArrays.paparonArray[i]);
-        }
 
-        kulinerHashMap.put("Bunto's Chicken & Rita Bakery", buntosList);
-        kulinerHashMap.put("Gethuk Goreng H. Tohirin Sokaraja", gethukList);
-        kulinerHashMap.put("Paparon's Pizza Purwokerto", paparonList);
+        kulinerHashMap.put("Wisata Kuliner", kulinerList);
         return kulinerHashMap;
     }
 
     public static HashMap<String, List<String>> perjalananDataHashMap() {
         HashMap<String, List<String>> perjalananHashMap = new HashMap<String, List<String>>();
-        List<String> amanatList = new ArrayList<String>();
-        List<String> arjunaList = new ArrayList<String>();
-        List<String> mitraList = new ArrayList<String>();
+        List<String> biroList = new ArrayList<String>();
 
 
-        for (int i = 0; i < MyDataArrays.amanatArray.length; i++) {
-            amanatList.add(MyDataArrays.amanatArray[i]);
+        for (int i = 0; i < MyDataArrays.biroArray.length; i++) {
+            biroList.add(MyDataArrays.biroArray[i]);
         }
 
-        for (int i = 0; i < MyDataArrays.arjunaArray.length; i++) {
-            arjunaList.add(MyDataArrays.arjunaArray[i]);
-        }
 
-        for (int i = 0; i < MyDataArrays.mitraArray.length; i++) {
-            mitraList.add(MyDataArrays.mitraArray[i]);
-        }
-
-        perjalananHashMap.put("Amanat Tour", amanatList);
-        perjalananHashMap.put("Arjuna Tour and Travel", arjunaList);
-        perjalananHashMap.put("CV Mitra Internusapada Tour & Travel", mitraList);
+        perjalananHashMap.put("Biro Perjalanan", biroList);
         return perjalananHashMap;
     }
 
